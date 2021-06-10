5846bfad64baa54ecd1e749ebf99841fde61cfef
Eric Tran <eric.tran119@gmail.com>
2021-06-09 23:56:22 -0400

    w8d4 homework

---
+++ W8D4/the_event_loop.js
@@ -0,0 +1,13 @@
+// /* Simple Timeout */
+
+window.setTimeout(function () {
+  alert('HAMMER TIME!')
+}, 5000);
+
+function hammerTime(time) {
+  window.setTimeout(function () {
+    alert(`${time} is HAMMER TIME!`);
+  });
+}
+
+
